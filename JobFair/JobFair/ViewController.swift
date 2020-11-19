7225c84928778143bbe02ff99b8d1efa6ab8b42c
Nguyễn Đình Thạch <shikanoji@gmail.com>
2020-11-19 18:25:47 +0700

    Init project

---
+++ JobFair/JobFair/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobFair
+//
+//  Created by Nguyễn Đình Thạch on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
